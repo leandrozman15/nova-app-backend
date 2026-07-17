99c4f1758ab94b922328668ca3733a055672189d
leandrozman15 <leozman15@gmail.com>
2026-07-17 23:32:39 +0000

    chore: remove compiled output from source control

--- dist/common/guards/firebase-auth.guard.d.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { FirebaseAdminService } from '../../firebase/firebase-admin.service';
-export declare class FirebaseAuthGuard implements CanActivate {
-    private readonly reflector;
-    private readonly firebaseAdminService;
-    constructor(reflector: Reflector, firebaseAdminService: FirebaseAdminService);
-    canActivate(context: ExecutionContext): Promise<boolean>;
-}
--- dist/firebase/firebase-admin.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Auth } from 'firebase-admin/auth';
-export declare class FirebaseAdminService implements OnModuleInit {
-    private readonly configService;
-    constructor(configService: ConfigService);
-    onModuleInit(): void;
-    get auth(): Auth;
-}
